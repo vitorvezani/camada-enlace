db4ecb6ee90ae5a85d6f1fee89c6f6c97163dbaa
Vitor Vezani <vitor.vezani@gmail.com>
2013-03-19 11:02:44 -0300

    meu primeiro commit

---
+++ enlace.c
@@ -0,0 +1,10 @@
+//
+//  enlace.c
+//
+//  Created by Vitor Vezani on 13/03/13.
+//  Copyright (c) 2013 Vitor Vezani. All rights reserved.
+//
+
+int enviar(void bla){
+	return 0;
+}
---
+++ teste_enlace.c
@@ -0,0 +1,13 @@
+//
+//  teste_enlace.c
+//
+//  Created by Vitor Vezani on 13/03/13.
+//  Copyright (c) 2013 Vitor Vezani. All rights reserved.
+//
+
+int int main(int argc, char const *argv[])
+{
+	int i;
+	
+	return 0;
+}
