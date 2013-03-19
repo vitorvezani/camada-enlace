45c9cbee92fa576996188f3c7a33bd5dfb611f51
Vitor Vezani <vitor.vezani@gmail.com>
2013-03-19 18:03:02 -0300

    Feito - Ler do arquivo

--- enlace.c
+++ enlace.c
@@ -5,6 +5,49 @@
 //  Copyright (c) 2013 Vitor Vezani. All rights reserved.
 //
 
-int enviar(int bla){
-	return 0;
+#include <stdio.h>
+#include <stdlib.h>
+
+struct package{
+    char nos[6][3];
+    int enlaces[18][3];
+};
+
+void lerArquivo(char * nome_arq){
+
+ 	int result;
+ 	long lSize;
+ 	char * buffer;
+	FILE * fp;
+	fp = fopen(nome_arq, "r");
+
+    if(!fp)
+        perror("Fopen()");
+
+	// obtain file size:
+	fseek (fp , 0 , SEEK_END);
+	lSize = ftell (fp);
+	rewind (fp);
+
+	// allocate memory to contain the whole file:
+	buffer = (char*) malloc (sizeof(char)*lSize);
+		if (buffer == NULL){
+			fputs ("Memory error",stderr); 
+			exit (2);
+		}
+
+    result = fread(buffer, 1, lSize, fp);
+	    if (result != lSize){
+	    	fputs ("Reading error",stderr);
+	    	exit (3);
+	    }
+
+	colocarArquivoStruct(buffer);
+
+	fclose (fp);
+  	free (buffer);
+
+}
+
+void colocarArquivoStruct(char * buffer){
 }
--- teste_enlace.c
+++ teste_enlace.c
@@ -5,9 +5,30 @@
 //  Copyright (c) 2013 Vitor Vezani. All rights reserved.
 //
 
-int int main(int argc, char const *argv[])
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+
+int main(int argc, char const *argv[])
 {
-	int i;
+	int num_no; 
+	char nome_arq[20];
+
+	if (argc != 3) {
+        printf("Use: %s 'nome_arq_config' 'numero_nó'\n", argv[0]);
+        exit(1);
+    }
+
+    strcpy(nome_arq,argv[1]);
+	num_no = atoi(argv[2]);
+
+	printf("nome do arquivo: '%s'\n num do nó: '%d'\n",nome_arq,num_no);
+
+	lerArquivo(nome_arq);
 	
 	return 0;
 }
